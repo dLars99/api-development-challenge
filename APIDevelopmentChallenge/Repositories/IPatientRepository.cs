af8b25ca1bf6d3249ba8542a01379571c275783d
David Larsen <davidplarsen@gmail.com>
2021-04-14 17:27:35 -0500

    Remove WeatherForecast

--- APIDevelopmentChallenge/Repositories/IPatientRepository.cs
+++ APIDevelopmentChallenge/Repositories/IPatientRepository.cs
@@ -1,9 +1,11 @@
 ﻿using APIDevelopmentChallenge.Models;
+using System.Collections.Generic;
 
 namespace APIDevelopmentChallenge.Repositories
 {
     public interface IPatientRepository
     {
         void Add(Patient patient);
+        List<Patient> Get();
     }
 }
